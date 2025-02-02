91212d4b4cd35a2894df369b58da9cc8ecc8dd42
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-02-03 00:22:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import './../styles/App.css'; // Assuming the CSS is already in place
 
+// Home page component
+const Home = () => {
+  return <h1>Welcome to the Home page!</h1>;
+};
+
+// About page component
+const About = () => {
+  return <h1>This is an application that demonstrates React Router.</h1>;
+};
+
+// Main App component
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div className="app">
+        {/* Navigation links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App
+        {/* Routing between Home and About pages */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
